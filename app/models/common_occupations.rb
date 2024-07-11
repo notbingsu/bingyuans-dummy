1b4c164db45643db67be7a742df71a88708661db
notbingsu <bingyuan007@gmail.com>
2024-07-11 09:33:00 +0800

    updated common occupations file

---
+++ app/models/common_occupations.rb
@@ -0,0 +1,4 @@
+# 50 common occupations
+def common_occupations
+  %w(accountant actor architect artist attorney author baker banker barber bartender biologist butcher carpenter cashier chef chemist coach construction\ worker consultant dentist designer doctor driver electrician engineer farmer firefighter flight\ attendant graphic\ designer hairdresser journalist judge librarian mechanic nurse pharmacist photographer pilot plumber police\ officer professor programmer receptionist salesperson scientist secretary teacher technician translator veterinarian waiter)
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,8 @@
-require_relative "/home/bingsu/bootcamp/app/common_occupations.rb"
+require_relative 'common_occupations'
 class Game < ApplicationRecord
   before_create :set_occupation
-  OCCUPATIONS = %w(accountant actor architect artist attorney author baker banker barber bartender biologist butcher carpenter cashier chef chemist coach construction\ worker consultant dentist designer doctor driver electrician engineer farmer firefighter flight\ attendant graphic\ designer hairdresser journalist judge librarian mechanic nurse pharmacist photographer pilot plumber police\ officer professor programmer receptionist salesperson scientist secretary teacher technician translator veterinarian waiter)
   def set_occupation
-    self.occupation = OCCUPATIONS.sample
+    self.occupation = common_occupations.sample
   end
 
   def validate_guess(guess)
